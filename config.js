852243d75b1e53a089256c99812e8786447d878a
Mortimer Henningsson <mortimer.henningsson@gmail.com>
2022-09-08 15:20:21 +0200

    Add config for Dockans Hamnkrog

--- config.js
+++ config.js
@@ -66,6 +66,218 @@ module.exports = {
             ]
         },
         {
+            name: "Dockans Hamnkrog",
+            active: true,
+            url: "https://dockanshamnkrog.se/lunchmeny/",
+            scraperRules: [
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Monday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[text()='Måndag']"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Tuesday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[text()='Tisdag']"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Wednesday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[text()='Onsdag']"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Thursday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[text()='Torsdag']"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Friday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[text()='Fredag']"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Monday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Veckans veg') and contains(text(), 'måndag')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens veg"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Tuesday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Veckans veg') and contains(text(), 'tisdag')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens veg"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Wednesday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Veckans veg') and contains(text(), 'onsdag')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens veg"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Thursday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Veckans veg') and contains(text(), 'torsdag')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens veg"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Daily,
+                    day: Days.Friday,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Veckans veg') and contains(text(), 'fredag')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dagens veg"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Weekly,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Veckans fisk')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Veckans fisk"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()[normalize-space()]"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Weekly,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Veckans kött')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Veckans kött"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                },
+                {
+                    type: ScraperRuleType.Weekly,
+                    items: {
+                        selectorType: SelectorType.XPath,
+                        selector: "//*[@id='wrapper']//p/strong[contains(text(), 'Dockans räkmacka')]"
+                    },
+                    label: {
+                        selectorType: SelectorType.Manual,
+                        selector: "Dockans räkmacka"
+                    },
+                    dish: {
+                        selectorType: SelectorType.XPath,
+                        selector: "./following::text()"
+                    }
+                }
+            ]
+        },
+        {
             name: "p2",
             active: true,
             url: "https://www.restaurangp2.se/lunch",
