fc337e1ca2fc59d5fa073317ed696537ac600c93
Mortimer Henningsson <mortimer.henningsson@nexergroup.com>
2023-04-06 09:14:24 +0200

    Output bucket name for static website and cleanup

--- infrastructure/modules/storage_static_website/main.tf
+++ infrastructure/modules/storage_static_website/main.tf
@@ -1,19 +1,3 @@
-/**
- * Copyright 2022 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 # [START storage_static_website_create_bucket_tf]
 # Create new storage bucket in the provided region
 # with standard storage class and settings for main_page_suffix and not_found_page
@@ -26,40 +10,16 @@ resource "google_storage_bucket" "static_website" {
 
   website {
     main_page_suffix = "index.html"
-    not_found_page   = "404.html"
+    not_found_page   = "index.html"
   }
 }
 # [END storage_static_website_create_bucket_tf]
 
 # # [START storage_static_website_make_bucket_public_tf]
 # # Make bucket public by granting allUsers READER access
-# resource "google_storage_bucket_access_control" "public_rule" {
-#   bucket = google_storage_bucket.static_website.id
-#   role   = "READER"
-#   entity = "allUsers"
-# }
-# # [END storage_static_website_make_bucket_public_tf]
 resource "google_storage_bucket_iam_member" "member" {
   bucket  = google_storage_bucket.static_website.id
   role    = "roles/storage.legacyObjectReader"
   member  = "allUsers"
-
 }
-
-# [START storage_static_website_upload_files_tf]
-# Upload a simple index.html page to the bucket
-resource "google_storage_bucket_object" "indexpage" {
-  name         = "index.html"
-  content      = "<html><body>Hello World!</body></html>"
-  content_type = "text/html"
-  bucket       = google_storage_bucket.static_website.id
-}
-
-# Upload a simple 404 / error page to the bucket
-resource "google_storage_bucket_object" "errorpage" {
-  name         = "404.html"
-  content      = "<html><body>404!</body></html>"
-  content_type = "text/html"
-  bucket       = google_storage_bucket.static_website.id
-}
-# [END storage_static_website_upload_files_tf]
+# # [END storage_static_website_make_bucket_public_tf]
